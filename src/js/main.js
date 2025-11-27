d5b55d9c3760a24074535331846cdd224a37b71f
TheLastOneNT <thelastonent@gmail.com>
2025-11-27 11:59:31 -0500

    update

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,36 @@ import { initUI } from "./ui.js";
 window.addEventListener("DOMContentLoaded", () => {
   initUI();
 });
+
+document.addEventListener(
+  "touchstart",
+  function (event) {
+    if (event.touches.length > 1) {
+      event.preventDefault();
+    }
+  },
+  { passive: false }
+);
+
+let lastTouchEnd = 0;
+document.addEventListener(
+  "touchend",
+  function (event) {
+    const now = Date.now();
+    if (now - lastTouchEnd <= 300) {
+      event.preventDefault();
+    }
+    lastTouchEnd = now;
+  },
+  { passive: false }
+);
+
+["gesturestart", "gesturechange", "gestureend"].forEach((type) => {
+  window.addEventListener(
+    type,
+    function (event) {
+      event.preventDefault();
+    },
+    { passive: false }
+  );
+});
